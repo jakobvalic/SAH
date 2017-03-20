8428b230935fac69c163407c42e8bbd54ceb34ef
Jakob Valič <jakob.valic@student.fmf.uni-lj.si>
2017-03-20 19:33:45 +0100

    premikanje kmeta (1/2) in trdnjave

--- figure.py
+++ figure.py
@@ -1,5 +1,15 @@
 # figure
 
+polozaj_lastnih = [(1, 1)]
+polozaj_nasprotnih = [(2,2), (4,4)]
+
+def lastne_ovire(zeljena_poteza, polozaj_lastnih):
+    '''Figura se ne sme premakniti na polje, ki je že zasedeno z eno od figur iste barve.'''
+    if zeljena_poteza in polozaj_lastnih:
+        return False
+    return True
+
+
 def je_v_polju(poteza):
     '''Preveri, ali je poteza v polju.'''
     x, y = poteza
@@ -29,8 +39,14 @@ class Konj():
         desno = x + 2
         mozne_p += [(levo, y-1), (levo, y+1)]
         mozne_p += [(desno, y-1), (desno, y+1)]
+##        for i in range(len(mozne_p)):
+##            poteza = mozne_p[i]
+##            if poteza in self.polozaj_lastnih:
+##                pass
+##            else:
+##                append           
         for poteza in mozne_p:
-            if je_v_polju(poteza):
+            if je_v_polju(poteza) and lastne_ovire(poteza, polozaj_lastnih):
                 veljavne_mozne.append(poteza)
         self.poteze.extend(veljavne_mozne)
         return veljavne_mozne
@@ -73,8 +89,10 @@ class Lovec():
                 mozne_veljavne.append((x - odmik, y - odmik)) # dol
             if y + odmik <= 7:
                 mozne_veljavne.append((x - odmik, y + odmik)) # gor
-            odmik += 1
-        self.poteze.extend(mozne_veljavne)
+            odmik += 1 # PREKINITI JE TREBA, ČE JE POT NAPREJ BLOKIRANA Z LASTNO ALI NASPROTNO FIGURO
+        for poteza in mozne_veljavne:
+            if lastne_ovire(poteza, polozaj_lastnih):
+                self.poteze.extend([poteza])
         return mozne_veljavne
 
 lovec = Lovec(polozaj)
@@ -85,11 +103,96 @@ print(polozaj2, lovec.poteze)
 
 lovec = Lovec(polozaj3)
 print(polozaj3, lovec.poteze)
-                
-            
-            
+
+class Kmet():
+    def __init__(self, polozaj, polozaj_lastnih, polozaj_nasprotnih):
+        self.polozaj = polozaj
+        self.polozaj_lastnih = polozaj_lastnih
+        self.polozaj_nasprotnih = polozaj_nasprotnih
+        self.poteze = []
+        self.mozne_poteze(polozaj)
+
+    def mozne_poteze(polozaj):
+        '''Vrne možne poteze kmeta.'''
+        # povsod bo treba preverjati, ali ne odkrijemo šaha
+        polozaj_lastnih = self.polozaj_lastnih # lahko pa dava v parametre funkcije: mozne_poteze(polozaj, polozaj_lastnih, polozaj_nasprotnih) in tedaj self... ni potreben
+        polozaj_nasprotnih = self.polozaj_nasprotnih
+        mozne = []
+        # premik za ena naprej
+        x, y = polozaj
+        # append in extend ne vračata ničesar, le spreminjata sezname; to je nevarno
+        if (x, y+1) not in polozaj_lastnih or (x, y+1) not in polozaj_nasprotnih:
+            mozne.append(x, y+1)
+        # da lahko poje levo ali desno
+        if (x+1, y+1) in polozaj_nasprotnih:
+            mozne.append(x+1, y+1)
+        if (x+1, y-1) in polozaj_nasprotih:
+            mozne.append(x+1, y+1)
+        # za dve naprej
+
+
+        # en-passant (mimogrede)
         
 
+class Trdnjava():
+    def __init__(self, polozaj, polozaj_lastnih, polozaj_nasprotnih):
+        self.poteze = [] # ali zaenkrat rabiva kaj več?
+        self.mozne_poteze(polozaj, polozaj_lastnih, polozaj_nasprotnih)
+        
+
+    def mozne_poteze(self, polozaj, polozaj_lastnih, polozaj_nasprotnih):
+        '''Vrne seznam vseh možnih potez trdnjave.'''
+        mozne = []
+        (x, y) = polozaj
+                                                                            # for i in range(2): # zaradi simetrije, za gor in dol bomo zamenjali vlogi x in y :)
+        # desno:
+        premik = 1        
+        while x + premik <= 7:
+            if (x + premik, y) in polozaj_lastnih:
+                break # blokirana linija
+            if (x + premik, y) in polozaj_nasprotnih:
+                mozne.append((x + premik, y))
+                break # polja za nasprotno figuro so blokirana
+            mozne.append((x + premik, y))
+            premik += 1
+        # levo:
+        premik = 1
+        while 0 <= x - premik:
+            if (x - premik, y) in polozaj_lastnih:
+                break
+            if (x - premik, y) in polozaj_nasprotnih:
+                mozne.append((x - premik, y))
+                break
+            mozne.append((x - premik, y))
+            premik += 1
+                                                                            # sedaj zamenjamo vlogi x in y :)
+                                                                            # (y, x) = polozaj
+        # gor:
+        premik = 1        
+        while y + premik <= 7:
+            if (x, y + premik) in polozaj_lastnih:
+                break # blokirana linija
+            if (x, y + premik) in polozaj_nasprotnih:
+                mozne.append((x, y + premik))
+                break # polja za nasprotno figuro so blokirana
+            mozne.append((x, y + premik))
+            premik += 1
+        # dol:
+        premik = 1
+        while 0 <= y - premik:
+            if (x, y - premik) in polozaj_lastnih:
+                break
+            if (x, y - premik) in polozaj_nasprotnih:
+                mozne.append((x, y - premik))
+                break
+            mozne.append((x, y - premik))
+            premik += 1                                                                            
+        self.poteze.extend(mozne)
+        return mozne
+            
+            
+trdnjava = Trdnjava(polozaj, polozaj_lastnih, polozaj_nasprotnih)
+print('\ntrdnjave\n', polozaj, trdnjava.poteze, sep = '')
 
 
 
@@ -102,4 +205,22 @@ print(polozaj3, lovec.poteze)
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
